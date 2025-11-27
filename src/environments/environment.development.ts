67fe5b4533331b05212c3449411ade14f7c3e2c7
islam-atef <74112916+islam-atef@users.noreply.github.com>
2025-11-27 20:22:29 +0200

    first step (11/27/2025)

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://localhost:7072/api/',
+  authBaseUrl: 'https://localhost:7072/api/auth',
+  deviceManagementBaseUrl: 'https://localhost:7072/api/DevicesAuth/',
+};
